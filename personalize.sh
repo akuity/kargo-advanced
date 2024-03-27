d7e5803cc4fd4b6bd42db3d1244702e86bf9cfd7
Jesse Suen <jesse@akuity.io>
2024-03-26 19:15:55 -0700

    fix: better personalize.sh
    Signed-off-by: Jesse Suen <jesse@akuity.io>

--- personalize.sh
+++ personalize.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "./personalize.sh <username>"
-  exit 1
-fi
+echo -n "Enter GitHub username/org: "
+read username
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${1}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${1}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+
+echo "Enter Argo CD destination name or server where applications will be deployed (e.g. in-cluster, https://kubernetes.default.svc)"
+echo -n "Destination: "
+read destination
+
+if [[ $destination == https:* ]]; then
+  sed -i.bak -E "s@^        (# )?server:.*@        server: ${destination}@g" argocd/appset.yaml
+  sed -i.bak -E "s@^        (# )?name:.*@        # name: REPLACEME@g" argocd/appset.yaml
+else
+  sed -i.bak -E "s@^        (# )?name:.*@        name: ${destination}@g" argocd/appset.yaml
+  sed -i.bak -E "s@^        (# )?server:.*@        # server: https://REPLACEME@g" argocd/appset.yaml
+fi
+rm -f argocd/appset.yaml.bak
